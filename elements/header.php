cb886b65064ef112911815fa86b9d0108a3c1fec
kkortes <kim@kortes.se>
2014-07-09 12:12:29 +0200

    added social sharing for the project

--- elements/header.php
+++ elements/header.php
@@ -34,14 +34,14 @@
 }(document, 'script', 'facebook-jssdk'));</script>
 <?=$app->get('parts/top')?>
 <div id="wrapper">
-	<div class="drop right top base-padding">
+	<div class="drop left top base-padding">
 		<div class="fb-like" data-href="http://leaf.kortes.se" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 
-		<div class="drop down-right" style="padding-top:20px; right:-20px;">
+		<div class="drop down-left base-padding" style="padding-top:25px;">
 			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://leaf.kortes.se" data-text="Check out Leaf CSS Framework!" data-via="kortekim" data-hashtags="materialdesign">Tweet</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-			<div class="drop down-right" style="padding-top:5px: right:-5px;">
+			<div class="drop down-left " style="padding-left:10px;">
 				<!-- Place this tag where you want the +1 button to render. -->
 				<div class="g-plusone" data-size="medium"></div>
 			</div>
