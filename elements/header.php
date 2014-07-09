f8a185fa5f4caf83585321f079c0e3c02b8e689d
kkortes <kim@kortes.se>
2014-07-09 13:20:40 +0200

    better twitter share

--- elements/header.php
+++ elements/header.php
@@ -38,7 +38,7 @@
 		<div class="fb-like" data-href="http://leaf.kortes.se" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 
 		<div class="drop down-left base-padding" style="padding-top:25px;">
-			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://leaf.kortes.se" data-text="Check out Leaf CSS Framework!" data-via="kortekim" data-hashtags="materialdesign">Tweet</a>
+			<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://leaf.kortes.se" data-text="Check out Leaf CSS Framework!" data-via="kortekim" data-hashtags="materialdesign, leaf">Tweet</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
 			<div class="drop down-left " style="padding-left:10px;">
