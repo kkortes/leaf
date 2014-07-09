88ca90591df9416468e4913a4b5cc8a86ddb8ea8
kkortes <kim@kortes.se>
2014-07-09 11:36:04 +0200

    added analytics

--- elements/header.php
+++ elements/header.php
@@ -14,5 +14,15 @@
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,500,500italic,400italic,300italic,300,100italic,100' rel='stylesheet' type='text/css' />
 </head>
 <body class="page-<?=(isset($_GET['page']) ? $_GET['page'] : 'index')?>">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52663932-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <?=$app->get('parts/top')?>
 <div id="wrapper">
